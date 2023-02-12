e146653bea77d5937bc26e064f9f5fca96d304d0
khanhtrinh2003 <nguyenduckhanhtrinh@gmail.com>
2023-02-12 16:37:03 +0700

    update

---
+++ operators.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+
+def rank(x):
+    return x.rank(axis=1,ascending=True)
+
+def ts_sum(x,d):
+    return x.rolling(d).sum()
+
+def ts_delta(x,d):
+    return x-x.shift(d)
+
+def ts_std(x,d):
+    return x.rolling(d).std()
+
+def ts_min(x,d):
+    return x.rolling(d).min()
+
+def ts_max(x,d):
+    return x.rolling(d).max()
+
+def ts_product(x,d):
+    return x.rolling(d).agg(np.prod)
+
+def ts_median(x, d):
+    return x.rolling(d).median()
+
+def ts_mean(x, d):
+    return x.rolling(d).mean()
+
+def ts_delay(x, d):
+    return x.shift(d)
+
+def ts_ir(x, d):
+    return ts_mean(x,d)/ts_std(x,d)
+
+def ts_rank(x,d):
+    return x.rolling(d).rank()
+
+def ts_skewness(x, d):
+    return x.rolling(d).skew()
+
+def ts_min_diff(x, d):
+    return x-ts_min(x,d)
+
+def ts_max_diff(x, d):
+    return x-ts_max(x,d)
+
+def ts_kurtosis(x, d):
+    return x.rolling(d).kurt()
+
+def z_score(x):
+    return (x-x.mean(axis=0))/x.std(axis=0)
+
+def ts_av_diff(x, d):
+    return x-ts_mean(x,d)
+
+def ts_moment(x, d, k=0):
+    def central_moment(x, **kwargs):
+        k = kwargs['k']
+        mean = x.mean()
+        centered = x - mean
+        return (centered**k).mean()
+    
+    return x.rolling(d).agg(central_moment, k=k)
+
+def ts_scale(x, d, constant = 0):
+    return (x - ts_min(x, d)) / (ts_max(x, d) - ts_min(x, d)) + constant
