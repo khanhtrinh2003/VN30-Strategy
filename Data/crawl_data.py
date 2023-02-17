e9139042677cf778e6346b39623fc2a1ce489c0a
khanhtrinh2003 <nguyenduckhanhtrinh@gmail.com>
2023-02-17 14:12:17 +0700

    update

---
+++ Data/crawl_data.py
@@ -0,0 +1,34 @@
+from vnstock import *
+from Tools.function import *
+from Tools.operators import *
+from Tools.technical import *
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+today = pd.to_datetime('today').date()
+formatted_date = today.strftime('%Y-%m-%d')
+print(formatted_date)
+
+ticket = ['ACB', 'BCM', 'BID', 'BVH', 'CTG', 'FPT', 'GAS', 'GVR', 'HDB', 'HPG', 'MBB', 'MSN', 'MWG', 'NVL', 'PDR', 'PLX', 'POW', 'SAB', 'SSI', 'STB', 'TCB', 'TPB', 'VCB', 'VHM', 'VIB', 'VIC', 'VJC', 'VNM', 'VPB', 'VRE']
+frame = []
+for i in ticket:
+    a = stock_historical_data(symbol=i, 
+                            start_date=formatted_date,
+                            end_date=formatted_date)
+    a["ticker"] = i
+    frame.append(a)
+    
+q = pd.concat(frame)
+close= q.pivot_table(values="Close", columns="ticker", index="TradingDate")
+high= q.pivot_table(values="High", columns="ticker", index="TradingDate")
+low= q.pivot_table(values="Low", columns="ticker", index="TradingDate")
+open= q.pivot_table(values="Open", columns="ticker", index="TradingDate")
+volume = q.pivot_table(values="Volume", columns="ticker", index="TradingDate")
+
+close= close.to_csv("Data/close.csv",mode="a",header=False)
+high= high.to_csv("Data/high.csv",mode="a",header=False)
+low= low.to_csv("Data/low.csv",mode="a",header=False)
+open= open.to_csv("Data/open.csv",mode="a",header=False)
+volume= volume.to_csv("Data/volume.csv",mode="a",header=False)
